cfa28bff15d462b8ff20242ad2b9f89d3eed9586
vlerm <mr.kooper.98@mail.ru>
2022-05-30 02:38:57 +0300

    Initial Commit

---
+++ fire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fire
+//
+//  Created by Вадим Лавор on 30.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
